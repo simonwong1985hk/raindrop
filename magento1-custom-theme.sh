f120eab1bf1224623e302f11d3856be433a86f38
Simon Wong <simonwong1985hk@gmail.com>
2019-08-10 12:43:07 +0800

    Update magento1-custom-theme.sh

--- magento1-custom-theme.sh
+++ magento1-custom-theme.sh
@@ -44,19 +44,8 @@ EOF
     rm -rf skin/frontend/$1/default/css/madisonisland.css
     rm -rf skin/frontend/$1/default/css/madisonisland-ie8.css
 
-    git clone https://github.com/simonwong1985hk/My_External.git
-
-    cp -R My_External/* .
-
-    rm -rf My_External
-
 	echo '<layout version="0.1.0">
     <default>
-        <!-- Set default page layout to 1 column -->
-        <reference name="root">
-            <action method="setTemplate"><template>page/1column.phtml</template></action>
-        </reference>
-
         <reference name="head">
             <!-- Removing sample data assets -->
             <action method="removeItem"><type>skin_js</type><name>js/slideshow.js</name></action>
@@ -64,58 +53,23 @@ EOF
             <action method="removeItem"><type>skin_js</type><name>js/lib/jquery.cycle2.swipe.min.js</name></action>
             <action method="removeItem"><type>skin_css</type><name>css/madisonisland.css</name></action>
             <action method="removeItem"><type>skin_css</type><name>css/madisonisland-ie8.css</name></action>
-            <action method="removeItem"><type>link_rel</type><name>//fonts.googleapis.com/css?family=Raleway:300,400,500,700,600</name></action>
+            <!-- <action method="removeItem"><type>link_rel</type><name>//fonts.googleapis.com/css?family=Raleway:300,400,500,700,600</name></action> -->
 
             <!-- Google fonts -->
-            <action method="addLinkRel"><rel>stylesheet</rel><href>//fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i|Noto+Serif:400,400i,700,700i</href></action>
+            <!-- <action method="addLinkRel"><rel>stylesheet</rel><href>//fonts.googleapis.com/css?family=NEW_FONT</href></action> -->
 
-            <!-- uikit.min.css -->
-            <action method="addExternalItem">
-                <type>external_css</type>
-                <name>https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/css/uikit.min.css</name>
-                <params/>
-            </action>
-
-            <!-- uikit.min.js -->
-            <action method="addExternalItem">
-                <type>external_js</type>
-                <name>https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/js/uikit.min.js</name>
-                <params/>
-            </action>
-
-            <!-- slideshow.min.js -->
-            <action method="addExternalItem">
-                <type>external_js</type>
-                <name>https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/js/components/slideshow.min.js</name>
-                <params/>
-            </action>
-
-            <!-- slider.min.js -->
-            <action method="addExternalItem">
-                <type>external_js</type>
-                <name>https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/js/components/slider.min.js</name>
-                <params/>
-            </action>
-
-            <!-- font-awesome.min.css -->
-            <action method="addExternalItem">
-                <type>external_css</type>
-                <name>https://use.fontawesome.com/releases/v5.3.1/css/all.css</name>
-                <params/>
-            </action>
-
-            <!-- custom.css -->
+            <!-- '"$1"'.css -->
             <action method="addItem">
                 <type>skin_css</type>
-                <name>custom/custom.css</name>
+                <name>'"$1"'/'"$1"'.css</name>
                 <params/>
                 <if><![CDATA[<!--[if (gte IE 9) | (IEMobile)]><!-->]]></if>
             </action>
 
-            <!-- custom.js -->
+            <!-- '"$1"'.js -->
             <action method="addItem">
                 <type>skin_js</type>
-                <name>custom/custom.js</name>
+                <name>'"$1"'/'"$1"'.js</name>
                 <params/>
             </action>
         </reference>
@@ -125,13 +79,13 @@ EOF
     </default>
 </layout>' > app/design/frontend/$1/default/layout/local.xml
 
-	mkdir -p skin/frontend/$1/default/custom
+	mkdir -p skin/frontend/$1/default/$1
 
-	touch skin/frontend/$1/default/custom/custom.css
+	touch skin/frontend/$1/default/$1/$1.css
 
-	touch skin/frontend/$1/default/custom/custom.js
+	touch skin/frontend/$1/default/$1/$1.js
 
-	sed -i '' 's/Raleway/Noto Sans/g' skin/frontend/$1/default/scss/_var.scss
+    # sed -i '' 's/Raleway/NEW_FONT/g' skin/frontend/$1/default/scss/_var.scss
 
 	compass compile skin/frontend/$1/default/scss
 
