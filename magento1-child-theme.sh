0022f4dd5c03b587886fea121a9bc8dc3a54b576
Simon Wong <simonwong1985hk@gmail.com>
2019-08-14 09:44:58 +0800

    Update magento1-child-theme.sh

--- magento1-child-theme.sh
+++ magento1-child-theme.sh
@@ -1,36 +1,38 @@
-if [ ! -z $1 ]; then
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ]; then
 
-	mkdir -p app/design/frontend/$1/default
+    echo '$1 = CHILD_THEME_NAME'
+    echo '$2 = DATABASE_NAME'
+    echo '$3 = DATABASE_USER'
+    echo '$4 = DATABASE_PASSWORD'
 
-	mkdir -p app/design/frontend/$1/default/etc
+else
 
-	echo '<?xml version="1.0"?>
+    mkdir -p app/design/frontend/$1/default
+
+    mkdir -p app/design/frontend/$1/default/etc
+
+    echo '<?xml version="1.0"?>
 <theme>
     <parent>rwd/default</parent>
 </theme>' > app/design/frontend/$1/default/etc/theme.xml
 
-	mkdir -p app/design/frontend/$1/default/layout
+    mkdir -p app/design/frontend/$1/default/layout
 
-	mkdir -p app/design/frontend/$1/default/template
+    mkdir -p app/design/frontend/$1/default/template
 
-	mkdir -p skin/frontend/$1/default
+    mkdir -p skin/frontend/$1/default
 
-	cp -R skin/frontend/rwd/default/images skin/frontend/$1/default
+    cp -R skin/frontend/rwd/default/images skin/frontend/$1/default
 
-	cp -R skin/frontend/rwd/default/scss skin/frontend/$1/default
+    cp -R skin/frontend/rwd/default/scss skin/frontend/$1/default
 
-	echo 'sourcemap = true' >> skin/frontend/$1/default/scss/config.rb
+    echo 'sourcemap = true' >> skin/frontend/$1/default/scss/config.rb
 
-	sed -i '' 's/production/development/g' skin/frontend/$1/default/scss/config.rb
+    sed -i '' 's/production/development/g' skin/frontend/$1/default/scss/config.rb
 
     # Disable Cache Management
 
-	mysql -u root -p"root" <<EOF
-
-    USE $2;
-
-    UPDATE core_cache_option SET value=0;
-EOF
+    mysql -u $3 -p"$4" -e "USE $2; UPDATE core_cache_option SET value=0;"
 
     php -r "require_once('app/Mage.php');umask(0);Mage::app();Mage::getConfig()->saveConfig('design/package/name', '$1', 'default', 0);"
 
@@ -44,7 +46,7 @@ EOF
     rm -rf skin/frontend/$1/default/css/madisonisland.css
     rm -rf skin/frontend/$1/default/css/madisonisland-ie8.css
 
-	echo '<layout version="0.1.0">
+    echo '<layout version="0.1.0">
     <default>
         <reference name="head">
             <!-- Removing sample data assets -->
@@ -79,19 +81,14 @@ EOF
     </default>
 </layout>' > app/design/frontend/$1/default/layout/local.xml
 
-	mkdir -p skin/frontend/$1/default/$1
+    mkdir -p skin/frontend/$1/default/$1
 
-	touch skin/frontend/$1/default/$1/$1.css
+    touch skin/frontend/$1/default/$1/$1.css
 
-	touch skin/frontend/$1/default/$1/$1.js
+    touch skin/frontend/$1/default/$1/$1.js
 
     # sed -i '' 's/Raleway/NEW_FONT/g' skin/frontend/$1/default/scss/_var.scss
 
-	compass compile skin/frontend/$1/default/scss
-
-else
-
-    echo '$1 = YOUR_THEME_NAME'
-    echo '$2 = YOUR_DATABASE_NAME'
+    compass compile skin/frontend/$1/default/scss
 
 fi
