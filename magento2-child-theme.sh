6c3599a86fe8df3caff7437f7fa48c204f8e31d6
Simon Wong <simonwong1985hk@gmail.com>
2019-08-14 09:47:56 +0800

    Update magento2-child-theme.sh

--- magento2-child-theme.sh
+++ magento2-child-theme.sh
@@ -1,19 +1,23 @@
-if [ ! -z $1 ]; then
+if [ -z $1 ]; then
 
-	# php bin/magento deploy:mode:set developer
+    echo '$1 = CHILD_THEME_NAME'
 
-	mkdir -p app/design/frontend/$1/$1
+else
 
-	path=app/design/frontend/$1/$1
+    # php bin/magento deploy:mode:set developer
 
-	# theme.xml
-	echo '<theme xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:Config/etc/theme.xsd">
+    mkdir -p app/design/frontend/$1/$1
+
+    path=app/design/frontend/$1/$1
+
+    # theme.xml
+    echo '<theme xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:Config/etc/theme.xsd">
      <title>'"$1"'</title>
      <parent>Magento/blank</parent>
 </theme>' > $path/theme.xml
 
-	# composer.json
-	echo '{
+    # composer.json
+    echo '{
     "name": "'$1'/theme-frontend-blank-child",
     "description": "'$1'",
     "config": {
@@ -36,31 +40,31 @@ if [ ! -z $1 ]; then
     }
 }' > $path/composer.json
 
-	# registration.php
-	echo "<?php
+    # registration.php
+    echo "<?php
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/$1/$1', __DIR__);" > $path/registration.php
 
 
-	# Directories
-	mkdir -p $path/web/css/source
+    # Directories
+    mkdir -p $path/web/css/source
 
-	mkdir -p $path/web/fonts
+    mkdir -p $path/web/fonts
 
-	mkdir -p $path/web/images
+    mkdir -p $path/web/images
 
-	mkdir -p $path/web/js
+    mkdir -p $path/web/js
 
-	# Clear Cache
-	rm -rf pub/static/*
+    # Clear Cache
+    rm -rf pub/static/*
 
-	rm -rf var/view_preprocessed/*
+    rm -rf var/view_preprocessed/*
 
-	# Declaring theme logo
-	mkdir -p $path/Magento_Theme/layout
+    # Declaring theme logo
+    mkdir -p $path/Magento_Theme/layout
 
-	echo '<page xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:View/Layout/etc/page_configuration.xsd">
+    echo '<page xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:View/Layout/etc/page_configuration.xsd">
     <body>
         <referenceBlock name="logo">
             <arguments>
@@ -72,10 +76,6 @@ ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/$1/$1', __DIR_
     </body>
 </page>' > $path/Magento_Theme/layout/default.xml
 
-	php bin/magento cache:flush
-
-else
-
-	echo '$1 = CHILD_THEME_NAME'
+    php bin/magento cache:flush
 
 fi
