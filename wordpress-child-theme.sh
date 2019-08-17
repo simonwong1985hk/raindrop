0bb2e3b5e597fef48b9c1a2d6badccf95b640236
Simon Wong <simonwong1985hk@gmail.com>
2019-08-17 11:41:06 +0800

    Update wordpress-child-theme.sh

--- wordpress-child-theme.sh
+++ wordpress-child-theme.sh
@@ -5,21 +5,21 @@ if [ -z $1 ]; then
 else
 
 	parent="./wp-content/themes/$1"
-	
+
 	child="./wp-content/themes/$1-child"
 
 	if [ -d $child ]; then
-	
+
 		rm -rf $child
-		
+
 	fi
 
 	mkdir -p $child
 
 	if [ -f $parent/screenshot* ]; then
-	
+
 		cp $parent/screenshot* $child/
-		
+
 	fi
 
 	echo "/*
@@ -32,11 +32,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     \$parent_style = '$1-style';
     wp_enqueue_style( \$parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( \$parent_style ),
-        wp_get_theme()->get('Version')
-    );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( \$parent_style ), wp_get_theme()->get('Version') );
 }" > $child/functions.php
 
 fi
