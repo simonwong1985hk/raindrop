3086bbe965f0d7c6b3ceb04de85b91f80b1b3233
Simon Wong <simonwong1985hk@gmail.com>
2019-08-24 12:52:02 +0800

    Update laravel-cashier.php

--- laravel-cashier.php
+++ laravel-cashier.php
@@ -19,42 +19,6 @@ CASHIER_CURRENCY=HKD
 CASHIER_CURRENCY_LOCALE=en_HK
 
 # frontend
-<input id="card-holder-name" type="text">
-
-<!-- Stripe Elements Placeholder -->
-<div id="card-element"></div>
-
-<button id="card-button">
-    Process Payment
-</button>
-
-<script src="https://js.stripe.com/v3/"></script>
-
-<script>
-    const stripe = Stripe('stripe-public-key');
-
-    const elements = stripe.elements();
-    const cardElement = elements.create('card');
-
-    cardElement.mount('#card-element');
-
-    const cardHolderName = document.getElementById('card-holder-name');
-    const cardButton = document.getElementById('card-button');
-
-    cardButton.addEventListener('click', async (e) => {
-        const { paymentMethod, error } = await stripe.createPaymentMethod(
-            'card', cardElement, {
-                billing_details: { name: cardHolderName.value }
-            }
-        );
-
-        if (error) {
-            // Display "error.message" to the user...
-        } else {
-            // The card has been verified successfully...
-        }
-    });
-</script>
 
 # backend
 try {
