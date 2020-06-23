2605f858eca2ecce5065f82c57c0e44b4a1345e8
Simon Wong <simonwong1985hk@gmail.com>
2020-06-23 17:19:04 +0800

    Update google-recaptcha-v3.php

--- google-recaptcha-v3.php
+++ google-recaptcha-v3.php
@@ -4,9 +4,7 @@
 <input type="hidden" name="recaptcha-response" id="recaptcha-response" />
 
 // Include the reCAPTCHA branding
-This site is protected by reCAPTCHA and the Google
-<a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and
-<a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
+This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
 
 /**
  * CSS
