94e83b7bbff8bc0fb97157e392542bcf1cb653f7
Simon Wong <simonwong1985hk@gmail.com>
2020-09-23 10:53:07 +0800

    Update laravel-seeding.php

--- laravel-seeding.php
+++ laravel-seeding.php
@@ -1,30 +1,65 @@
-php artisan make:factory UserFactory --model=User
-$factory->define(User::class, function (Faker $faker) {
+<?php
+/**
+ * php artisan make:factory UserFactory --model=User
+ */
+use App\Models\User;
+use Illuminate\Support\Str;
+
+public function definition()
+{
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $this->faker->name,
+        'email' => $this->faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => bcrypt('passw0rd'),
+        'password' => bcrypt('PASSWORD'),
         'remember_token' => Str::random(10),
     ];
-});
-
-php artisan make:seeder UsersTableSeeder
-public function run()
-{
-    factory(App\User::class, 500)->create();
 }
 
-# /database/seeds/DatabaseSeeder.php
+
+
+
+/**
+ * php artisan make:seeder UserSeeder
+ */
+use App\Models\User;
+
 public function run()
 {
+    User::truncate();
+
+    factory(User::class, 500)->create();
+}
+
+
+
+
+/**
+ * /database/seeds/DatabaseSeeder.php
+ */
+use Illuminate\Support\Facades\Schema;
+
+public function run()
+{
+    Schema::disableForeignKeyConstraints();
+
     $this->call([
-        UsersTableSeeder::class,
+        UserSeeder::class,
     ]);
+
+    Schema::enableForeignKeyConstraints();
 }
 
-composer dump-autoload
 
-php artisan db:seed
 
-php artisan migrate:refresh --seed
+
+/**
+ * composer dump-autoload
+ */
+
+
+
+
+/**
+ * php artisan db:seed
+ */
