75db038f84e53b975e61de9eb32e5b561a001d63
Simon Wong <simonwong1985hk@gmail.com>
2020-09-23 12:42:24 +0800

    Update laravel-full-text-search.php

--- laravel-full-text-search.php
+++ laravel-full-text-search.php
@@ -55,7 +55,7 @@ trait FullTextSearch
     {
         $columns = implode(',', $this->searchable);
 
-        $query->whereRaw("MATCH ({$columns}) AGAINST (? IN NATURAL LANGUAGE MODE)" , $this->fullTextWildcards($term));
+        $query->whereRaw("MATCH ({$columns}) AGAINST (? IN BOOLEAN MODE)" , $this->fullTextWildcards($term));
 
         return $query;
     }
