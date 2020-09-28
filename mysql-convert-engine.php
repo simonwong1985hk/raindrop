eadd3963007ad431e2ce52c3b60bc5c9df1c0fb6
Simon Wong <simonwong1985hk@gmail.com>
2020-09-28 15:52:35 +0800

    Create mysql-convert-engine.php

---
+++ mysql-convert-engine.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * /etc/my.cnf
+ * sql_mode=
+ */
+
+$dbname = '';
+$username = '';
+$password = '';
+
+$conn = new mysqli('localhost', $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+$sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" . $dbname . "' AND ENGINE = 'MyISAM'";
+
+$result = $conn->query($sql);
+
+while($row = $result->fetch_row())
+{
+    $table = $row[0];
+    $sql = "ALTER TABLE `$table` ENGINE=InnoDB";
+    $conn->query($sql);
+}
+
+$result->free();
+
+$conn->close();
+
+echo "Connected successfully" . PHP_EOL;
