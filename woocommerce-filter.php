27fa120caca563488537c68ddd4a6f44ae574e1d
Simon Wong <simonwong1985hk@gmail.com>
2020-09-30 09:50:04 +0800

    Update woocommerce-filter.php

--- woocommerce-filter.php
+++ woocommerce-filter.php
@@ -3,10 +3,17 @@
 /**
  * Change [Select options] for variable product
  */
-add_filter( 'woocommerce_product_add_to_cart_text', function( $text ) {
+add_filter( 'woocommerce_product_add_to_cart_text', function ( $text ) {
   global $product;
   if ( $product->is_type( 'variable' ) ) {
-    $text = $product->is_purchasable() ? __( 'CLICK TO VIEW MORE OPTIONS', 'woocommerce' ) : __( 'READ MORE', 'woocommerce' );
+    $text = $product->is_purchasable() ? __( 'CUSTOM TEXT', 'woocommerce' ) : __( 'CUSTOM TEXT', 'woocommerce' );
   }
   return $text;
 }, 10 );
+
+/**
+ * Change [Sale!] for on-sale product
+ */
+add_filter( 'woocommerce_sale_flash', function ( $html ) {
+	return str_replace( __( 'Sale!', 'woocommerce' ), __( 'CUSTOM TEXT', 'woocommerce' ), $html );
+} );
