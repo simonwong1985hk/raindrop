3489f063f5b31497d24ee3675cd62788368cafcb
Simon Wong <simonwong1985hk@gmail.com>
2020-09-30 09:14:05 +0800

    Create woocommerce-filter.php

---
+++ woocommerce-filter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Change [Select Options] for variable product
+ */
+add_filter( 'woocommerce_product_add_to_cart_text' , 'custom_woocommerce_product_add_to_cart_text' );
+function custom_woocommerce_product_add_to_cart_text() {
+    global $product;
+    $product_type = $product->product_type;
+    switch ( $product_type ) {
+    case 'variable':
+            return __( 'Click to view more options', 'woocommerce' );
+            break;
+    }
+}
