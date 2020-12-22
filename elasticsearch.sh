35d020eea1908c81429b789c2e4989e6efe4ef69
Simon Wong <simonwong1985hk@gmail.com>
2020-12-22 15:13:20 +0800

    Update elasticsearch.sh

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,27 +1,26 @@
-# Install Elasticsearch with RPM
+#!/bin/bash
 
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-vi /etc/yum.repos.d/elasticsearch.repo
-[elasticsearch]
+echo '[elasticsearch]
 name=Elasticsearch repository for 7.x packages
 baseurl=https://artifacts.elastic.co/packages/7.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=0
 autorefresh=1
-type=rpm-md
+type=rpm-md' >> /etc/yum.repos.d/elasticsearch.repo
 
-yum install --enablerepo=elasticsearch elasticsearch
+yum install -y --enablerepo=elasticsearch elasticsearch
 
-vi /etc/sysconfig/elasticsearch
-ES_JAVA_OPTS="-Djna.tmpdir=/var/lib/elasticsearch/tmp"
+echo 'ES_JAVA_OPTS="-Djna.tmpdir=/var/lib/elasticsearch/tmp"' >> /etc/sysconfig/elasticsearch
 
 mkdir -p /var/lib/elasticsearch/tmp
 
 chown -R elasticsearch:elasticsearch /var/lib/elasticsearch/tmp/
 
 systemctl daemon-reload
+
 systemctl enable elasticsearch.service
 
 systemctl start elasticsearch.service
